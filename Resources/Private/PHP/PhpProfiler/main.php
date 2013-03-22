432497f488a447c505f6f7569a3e25d566a26b91
Michael Knoll <knoll@punkt.de>
2013-03-22 15:29:07 +0100

    ADD: data mapper is now part of pt_solr CHG: Added configuration for pt_solr data mapper in solr.ts

--- Resources/Private/PHP/PhpProfiler/main.php
+++ Resources/Private/PHP/PhpProfiler/main.php
@@ -1,7 +1,11 @@
 <?php
 
+/**
+ * This file can be included to include all dependent class files.
+ * We are using the profiler very early in T3's bootstrap, so AutoLoading
+ * won't be available!
+ */
+
 require('Classes/Profiler.php');
 require('Classes/Domain/Model/EmptyProfilingRun.php');
 require('Classes/Domain/Model/ProfilingRun.php');
-
-?>
